091d320747071edd9c3bb086e70a0017cf122cc6
Luca Manuel De Veintemilla Altamirano <lmde2@espe.edu.ec>
2023-11-26 18:18:31 -0500

    Ejercicio2

--- app/src/main/java/com/example/prylog/Pantalla3.java
+++ app/src/main/java/com/example/prylog/Pantalla3.java
@@ -11,16 +11,17 @@ import android.widget.TextView;
 public class Pantalla3 extends AppCompatActivity {
 
     TextView lblResultado1, lblResultado2;
-    EditText txtNumero;
-    Button btn_calcular;
+    EditText txtNumero,txtPrimo;
+    Button btn_calcular,btn_esPrimo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
         txtNumero= findViewById(R.id.et_numero);
+        txtPrimo = findViewById(R.id.et_primo);
         btn_calcular  = findViewById(R.id.bt_calcular);
-
+        btn_esPrimo= findViewById(R.id.bt_esPrimo);
         lblResultado1 = findViewById(R.id.tv_resultado1);
         lblResultado2 = findViewById(R.id.tv_resultado2);
 
@@ -38,6 +39,35 @@ public class Pantalla3 extends AppCompatActivity {
 
                 }
                 lblResultado1.setText("Bienvenido, " + nombre +   " tu numero factorial es: "+factorial);
+            }
+        });
+
+        btn_esPrimo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num2 = txtPrimo.getText().toString();
+
+                if (!num2.isEmpty()) {
+                    int primo = Integer.parseInt(num2);
+
+                    // Verificar si el número es primo
+                    boolean esPrimo = true;
+                    for (int i = 2; i <= Math.sqrt(primo); i++) {
+                        if (primo % i == 0) {
+                            esPrimo = false;
+                            break; // No es necesario seguir verificando si ya encontramos un divisor
+                        }
+                    }
+
+                    // Muestra el resultado
+                    if (esPrimo) {
+                        lblResultado2.setText("El número " + primo + " es primo.");
+                    } else {
+                        lblResultado2.setText("El número " + primo + " no es primo.");
+                    }
+                } else {
+                    lblResultado2.setText("Por favor, ingresa un número antes de verificar.");
+                }
             }
         });
 
