204aa6ffff3bdb3b2f2b5aeaf4ebd2d47dc9b67e
JuanFReyes <juanfreyes17@gmail.com>
2023-11-26 18:23:42 -0500

    Pantalla 4

--- app/src/main/java/com/example/prylog/Pantalla4.java
+++ app/src/main/java/com/example/prylog/Pantalla4.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class Pantalla4 extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
