a4f74257c738894196f2c542f5ff5923cabf3796
Luca Manuel De Veintemilla Altamirano <lmde2@espe.edu.ec>
2023-11-27 08:30:20 -0500

    ProgressBar

--- app/src/main/java/com/example/prylog/MainActivity.java
+++ app/src/main/java/com/example/prylog/MainActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     //Declarar los objetos gráficos
     EditText txt_usuario,txt_clave;
     Button btn_login;
 
+    ProgressBar progressBar;
+    int counter=0;
     private String user ="Admin",clave1 ="AdminJuan";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         txt_usuario = findViewById(R.id.et_usuario);
         txt_clave = findViewById(R.id.et_contrasena);
         btn_login = findViewById(R.id.bt_login);
+        progressBar = findViewById(R.id.progressBar); // Add this line to initialize progressBar
+
+        progressBar.setVisibility(View.VISIBLE);
+
 
         //método onclick
         btn_login.setOnClickListener(new View.OnClickListener() {
@@ -31,31 +41,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String usuario = txt_usuario.getText().toString();
                 String clave = txt_clave.getText().toString();
-                if (usuario.length()!=0) {
-                    if (usuario.equals(user)) {
-                        //verificacion contraseña
-                        if(clave.equals(clave1)){
-                            Toast.makeText(MainActivity.this, "Datos correctos", Toast.LENGTH_SHORT).show();
-                            //conexion a otra actividad
-                            Intent conec= new Intent(MainActivity.this,Bienvenido.class);
 
-                            startActivity(conec);
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        counter++;
 
-                        }else{
-                            Toast.makeText(MainActivity.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
-
-                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressBar.setProgress(counter);
+                                if (counter == 100) {
+                                    timer.cancel();
+                                    if (usuario.length() != 0) {
+                                        if (usuario.equals(user)) {
+                                            // Verificación contraseña
+                                            if (clave.equals(clave1)) {
+                                                Toast.makeText(MainActivity.this, "Datos correctos", Toast.LENGTH_SHORT).show();
+                                                // Conexión a otra actividad
+                                                Intent conec = new Intent(MainActivity.this, Bienvenido.class);
+                                                startActivity(conec);
+                                            } else {
+                                                Toast.makeText(MainActivity.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                                            }
+                                        } else {
+                                            Toast.makeText(MainActivity.this, "Usuario incorrecta", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Datos incompletos", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
                     }
-                    else{
-                        Toast.makeText(MainActivity.this, "Usuario incorrecta", Toast.LENGTH_SHORT).show();
+                };
 
-                    }
-                }else{
-                    Toast.makeText(MainActivity.this, "Datos incompletos", Toast.LENGTH_SHORT).show();
-
-                }
+                timer.schedule(timerTask, 50, 50);
             }
         });
+
 
 
 
